2c24db5fb91afdba08a35e603dfb48b8554b1c0e
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2020-08-21 06:34:36 -0500

    Minor Fixes
    Minor Fixes

--- customers-api-traditional/src/main/java/org/ecuadorjug/customers/traditional/data/repository/CustomerRepository.java
+++ customers-api-traditional/src/main/java/org/ecuadorjug/customers/traditional/data/repository/CustomerRepository.java
@@ -4,4 +4,5 @@ import org.ecuadorjug.customers.traditional.data.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+    Customer findByName(String name);
 }
